1880468a9fe5c8fce20d4b3562b8d1ce666b88d7
alsanhani <alsanhani@users.noreply.github.com>
2026-08-25 22:42:20 +0300

    feat: initialize erpnext enterprise app without secrets

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="my_custom_app",
     version="1.0.0",
-    description="Enterprise ERPNext Custom Application",
     packages=find_packages(),
-    zip_safe=False,
     include_package_data=True,
     install_requires=["frappe"],
 )
